f2516ac9a135c37c5f9a3646df7f30cf609e0f97
JayaVeeramani <133194321+JayaVeeramani@users.noreply.github.com>
2023-08-04 15:17:22 +0530

    Added Plugins & image in home page meta tag

---
+++ assets/js/32.784d9a54.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{339:function(t,s,n){"use strict";n.r(s);var e=n(14),a=Object(e.a)({},(function(){var t=this._self._c;return t("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[t("img",{staticClass:"meta-image",attrs:{src:"/favicon.png"}}),this._v(" "),t("homePage")],1)}),[],!1,null,null,null);s.default=a.exports}}]);
